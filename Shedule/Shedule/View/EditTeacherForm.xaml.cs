7e10a0c10cd55cab59506c902b6b29a03a0514e8
nick <nrudov@gmail.com>
2013-02-25 14:51:12 +0300

    форма редактирования инф. о преподавателе

---
+++ Shedule/Shedule/View/EditTeacherForm.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using Shedule.ViewModel;
+
+namespace Shedule.View
+{
+    /// <summary>
+    /// Interaction logic for EditTeacherForm.xaml
+    /// </summary>
+    public partial class EditTeacherForm : Window
+    {
+        public EditTeacherForm()
+        {
+            InitializeComponent();
+            this.DataContext = new EditTeacherForm();
+        }
+    }
+}
---
+++ Shedule/Shedule/ViewModel/EditTeacherViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.IO;
+
+using Shedule.Commands;
+using Shedule.Import;
+using Shedule.Data;
+using System.Collections.ObjectModel;
+using System.Data.Objects.SqlClient;
+using Shedule.Common;
+
+namespace Shedule.ViewModel
+{
+    class EditTeacherViewModel : ViewModelBase
+    {
+    }
+}
